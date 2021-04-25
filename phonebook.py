a03b203b955d6a74790bb2711548e183722fb750
gilbertekalea <gilbertekalea@users.noreply.github.com>
2021-04-25 16:55:30 -0400

    update the delete funtionality

--- phonebook.py
+++ phonebook.py
@@ -1,55 +1,66 @@
-import csv, sys
+import csv
+import sys
 import datetime
 
-#create contact list
+# create contact list
+
+
 def AddContact():
     name = input("name: ").strip()
     number = int(input("phonenumber: ").strip())
     email = input("email: ").strip()
     address = input("address: ").strip()
 
-    #store input in a dictionary format
-    contactInfor = dict(name=name, number=number, email=email, address=address, date_created=datetime.date.today())
-    
-   #then store that same information in a csv file. 
+    # store input in a dictionary format
+    contactInfor = dict(name=name, number=number, email=email,
+                        address=address, date_created=datetime.date.today())
+
+   # then store that same information in a csv file.
     with open('phonedata.csv', 'a', newline='') as contact:
         fieldnames = ['name', 'number', 'email', 'address', 'date_created']
         writer = csv.DictWriter(contact, fieldnames=fieldnames)
         writer.writerow(contactInfor)
 
-    #Avoid reruning the scripting, ask users to if they would like to add another contact:
+    # Avoid reruning the script, ask users if they would like to add another contact:
     add_query = input("Do you want to search something else? Y/N: ")
     if add_query == 'Y':
-        add= AddContact()
+        add = AddContact()
     elif add_query == 'N':
         print(("*")*20, end=None)
         print("You are logged out")
         print(("*")*20, end=None)
-        print("Thank you for using our services")
+        print("Thank you for using our service")
         sys.exit()
     return add
 
-#Searching for contact: by name, number, email, address or date
+# Searching for contact: by name, number, email, address or date
+# Linear search
+
+
 def ContactSearch():
-    with open('phonedata.csv', newline='') as file:  #Open the csv file containing list of contacts:
-        reader = list(csv.DictReader(file))  #convert the csv into readerable dict
+    # Open the csv file containing list of contacts:
+    with open('phonedata.csv', newline='') as file:
+        # convert the csv into readerable dict
+        reader = list(csv.DictReader(file))
         q = input("Enter name or Number: ")  # search query :
         for i in range(0, len(reader)):
-            if q in reader[i].values(): # q can be name, number, email, address
-                print(("#")*20, "Printing Results", ("#")*20,end=None)
-                result = reader[i].items() #return items where the query matched.
+            if q in reader[i].values():  # q can be name, number, email, address
+                print(("#")*20, "Printing Results", ("#")*20, end=None)
+                # return items where the query matched.
+                result = reader[i].items()
                 print(result)
                 print(("#")*20, "done Printing", ("#")*20, end=None)
                 break
-            if q not in reader[i].values(): #Avoid printing not found anytime q is not found in reader[i].
-                #Until the program loops through the whole dict, then print the not found
+            # Avoid printing not found anytime q is not found in reader[i].
+            if q not in reader[i].values():
+                # Until the program loops through the whole dict, then print the not found
                 continue
         else:
             print(("#")*20, end=None)
             print("Please the contact can't be found!!")
             print(("#")*20, end=None)
 
-            #asking for more actions
+            # asking for more actions
             search_query = input("Do you want to search something else? Y/N: ")
             if search_query == 'Y':
                 search = ContactSearch()
@@ -63,24 +74,37 @@ def ContactSearch():
                 print("Thank you for using our services")
                 sys.exit()
             return search
+
 def contactdelete():
+    lines = list()
+    members = input("Please enter a number to be deleted")
     with open('phonedata.csv', newline='') as delfile:
-        file_del = list(csv.DictReader(delfile))
-        for i in range(0, len(file_del)):
-            delquery = input("What would you like to delete:")
-            if delquery in file_del[i].values():
-                delete = file_del[i].values()
-                del delete
-                sys.exit()
-                
+        reader = csv.reader(delfile)
+        for row in reader:
+            lines.append(row)
+            for field in row:
+                if field == members:
+                    lines.remove(row)
+
+    # Updating the CSV file.
+    with open("phonebook.csv", 'w', newline="") as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerow(lines)
+        print("deleted, Thank you")
+        print(lines)
+        sys.exit()
+
+
 def welcome():
     question = input(
-        "Reply C to Create contact or S for search: ")
+        "How can I help? C: create contact, S: for search, D: for delete. ")
     if question == "C":
         follow = AddContact()
     elif question == "S":
         follow = ContactSearch()
-    elif question=="D":
+    elif question == "D":
         follow = contactdelete()
     return follow
+
+
 welcome()
